b85fbe0efe774bdd9d298faf8148cdd26b4a4505
LazaroQueiroz <lazaro.queiroz.nascimento@ccc.ufcg.edu.br>
2025-12-10 00:24:13 -0300

    feat: adding event creation in files

--- novo/server/src/app.module.ts
+++ novo/server/src/app.module.ts
@@ -1,7 +1,10 @@
+import { CreateClassDto } from './dto/class/create-class.dto'
+import { ClassController } from './controller/class/class.controller'
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
+  imports: [ CreateClassDto ],
   providers: [],
+  controllers: [ ClassController ],
 })
 export class AppModule {}
